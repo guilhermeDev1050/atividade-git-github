59d834da93f8efdbb60542233a9d30750746f6d6
guilhermeDev1050 <guilhermejunco@hotmail.com>
2026-06-11 21:32:34 -0300

    feat: adiciona array de produtos do cardapio

--- cardapio.js
+++ cardapio.js
@@ -2,6 +2,13 @@
 
 console.log("Cardápio carregado com sucesso!");
 
+// Array de itens do cardápio (Questão 10b)
+const itensCardapio = [
+    { id: 1, nome: "X-Burguer", preco: 18.90, categoria: "Lanches" },
+    { id: 2, nome: "X-Bacon", preco: 21.90, categoria: "Lanches" },
+    { id: 3, nome: "Combo Família", preco: 45.90, categoria: "Combos" }
+];
+
 function fazerPedido(itemNome) {
     alert(`Pedido recebido! Você escolheu: ${itemNome}. Seu lanche já está sendo preparado! 🍔🚀`);
 }
