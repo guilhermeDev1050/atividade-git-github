24c79fcd0353bf5fa9633484a92446c239381b10
guilhermeDev1050 <guilhermejunco@hotmail.com>
2026-06-11 21:14:53 -0300

    feat: adiciona comportamento base ao cardapio

--- cardapio.js
+++ cardapio.js
@@ -1,1 +1,7 @@
-// cardapio.js
+// cardapio.js - Lógica interativa da Lanchonete do Bairro
+
+console.log("Cardápio carregado com sucesso!");
+
+function fazerPedido(itemNome) {
+    alert(`Pedido recebido! Você escolheu: ${itemNome}. Seu lanche já está sendo preparado! 🍔🚀`);
+}
